4baf7897e3ceb912fabe59dc73434f91e1035929
Thomas <t.pradie@gmail.com>
2014-02-20 00:17:21 +0100

    def sport affinitaires interview

--- pays_de_la_loire.fr/dataviz12/popupbox/popup_sources.js
+++ pays_de_la_loire.fr/dataviz12/popupbox/popup_sources.js
@@ -8,7 +8,7 @@
 
             // Binding a click event
             // From jQuery v.1.7.0 use .on() instead of .bind()
-            $('#my-button').bind('click', function(e) {
+            $('#defAffinitaires').bind('click', function(e) {
 
                 // Prevents the default action to be triggered. 
                 e.preventDefault();
