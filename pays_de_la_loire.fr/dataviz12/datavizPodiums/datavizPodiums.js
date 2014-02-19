9799c0898c23b92c5fdb7f49bf1b12e0234ab693
ptitFicus <benji.cavy@gmail.com>
2014-02-19 21:05:20 +0100

    Ajout des podiums au format svg

--- pays_de_la_loire.fr/dataviz12/datavizPodiums/datavizPodiums.js
+++ pays_de_la_loire.fr/dataviz12/datavizPodiums/datavizPodiums.js
@@ -8,7 +8,7 @@
                         
         $('#img2009').bind('click', function(e) {
             e.preventDefault();
-            document.getElementById("imagePodium").src= "img/podiums/2009.png";
+            document.getElementById("imagePodium").src= "img/podiums/2009.svg";
             document.getElementById("img2009").src= "img/podiums/c2009.png";
             document.getElementById("img2010").src= "img/podiums/nc2010.png";
             document.getElementById("img2011").src= "img/podiums/nc2011.png";
@@ -18,7 +18,7 @@
 
         $('#img2010').bind('click', function(e) {
             e.preventDefault();
-            document.getElementById("imagePodium").src= "img/podiums/2010.png";
+            document.getElementById("imagePodium").src= "img/podiums/2010.svg";
             document.getElementById("img2009").src= "img/podiums/nc2009.png";
             document.getElementById("img2010").src= "img/podiums/c2010.png";
             document.getElementById("img2011").src= "img/podiums/nc2011.png";
@@ -27,7 +27,7 @@
         
         $('#img2011').bind('click', function(e) {
             e.preventDefault();
-            document.getElementById("imagePodium").src= "img/podiums/2011.png";
+            document.getElementById("imagePodium").src= "img/podiums/2011.svg";
             document.getElementById("img2009").src= "img/podiums/nc2009.png";
             document.getElementById("img2010").src= "img/podiums/nc2010.png";
             document.getElementById("img2011").src= "img/podiums/c2011.png";
@@ -36,7 +36,7 @@
         
         $('#img2012').bind('click', function(e) {
             e.preventDefault();
-            document.getElementById("imagePodium").src= "img/podiums/2012.png";
+            document.getElementById("imagePodium").src= "img/podiums/2012.svg";
             document.getElementById("img2009").src= "img/podiums/nc2009.png";
             document.getElementById("img2010").src= "img/podiums/nc2010.png";
             document.getElementById("img2011").src= "img/podiums/nc2011.png";
