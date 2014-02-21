daa704595e08b91d52f7ac1a9f61bb0395f8b640
ptitFicus <benji.cavy@gmail.com>
2014-02-21 16:42:45 +0100

    dataviz podium ok à jslint

--- pays_de_la_loire.fr/dataviz/datavizPodiums/datavizPodiums.js
+++ pays_de_la_loire.fr/dataviz/datavizPodiums/datavizPodiums.js
@@ -4,19 +4,20 @@
 /** 
  * Change l'année de la dataviz des podiums
  */
-function changerAnneePodium (annee, interval) {
+function changerAnneePodium(annee, interval) {
+    'use strict';
     annee = parseInt(annee, 10);
     var debut = parseInt(interval[0], 10),
         fin = parseInt(interval[1], 10),
         actuel = debut,
         prefixe;
-    document.getElementById("imagePodium").src= "img/podiums/" + annee + ".svg";
+    document.getElementById("imagePodium").src = "img/podiums/" + annee + ".svg";
     for (actuel = debut; actuel <= fin; actuel += 1) {
-        if(actuel === annee) {
+        if (actuel === annee) {
             prefixe = "c";
         } else {
             prefixe = "nc";
         }
-        document.getElementById("img"+actuel).src= "img/podiums/" + prefixe + actuel.toString() + ".png";
+        document.getElementById("img" + actuel).src = "img/podiums/" + prefixe + actuel.toString() + ".png";
     }
 }
